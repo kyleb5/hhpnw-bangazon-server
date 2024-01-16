d9532a444e0698a08f71bf31b503e70d0ec2b213
Kyle B <kberry1298@gmail.com>
2024-01-16 14:36:55 -0600

    Changed date to integer for better timestamps

--- hhpnwapi/models/orders.py
+++ hhpnwapi/models/orders.py
@@ -5,7 +5,7 @@ class Orders(models.Model):
 
     customerEmail = models.EmailField()
     customerPhone = models.IntegerField()
-    date = models.DateField()
+    date = models.IntegerField()
     open = models.BooleanField()
     orderName = models.CharField(max_length=12)
     orderType = models.CharField(max_length=12)
